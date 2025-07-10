ed981d7a9dbfba99583ca24a2eaf5947955e8a99
Jan Pardo <Jan Pardo@LAPTOP-G6PGDB8U>
2025-07-10 09:21:54 -0700

    commi para agregar la clase conexion principalmente

--- src/clases/Grupo.java
+++ src/clases/Grupo.java
@@ -9,5 +9,41 @@ package clases;
  * @author raulr
  */
 public class Grupo {
+      public int idGrupo;
+    public String nombreGrupo;
+    public String estatus;
+    public int idCarrera;
+    
+    //Constructor de tarea
+
+    public Grupo(String nombreGrupo, String estatus, int idCarrera) {
+        this.nombreGrupo = nombreGrupo;
+        this.estatus = estatus;
+        this.idCarrera = idCarrera;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
     
 }
