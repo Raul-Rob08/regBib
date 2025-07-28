59e21ad9a1fb1286485b827a391e02d01bfbd790
Jan Pardo <Jan Pardo@LAPTOP-G6PGDB8U>
2025-07-28 05:53:31 -0700

    Este commit es de prueba para ver si puedo subir cambios, NO LO AGREGUES AL REPOSITORIO, VA ESTE ARCHIVO null151117796 Y quien sabe que sea NO LO ACEPTES ES DE CALE

--- src/frames/RegistrarDocente.java
+++ src/frames/RegistrarDocente.java
@@ -134,7 +134,9 @@ public class RegistrarDocente extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 412, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
